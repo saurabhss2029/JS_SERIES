daed361918d2321c94bd62659af116ee3456237a
Saurabh Kumar Kashinwar <skashinwar@gmail.com>
2025-09-02 12:02:06 +0530

    made some changes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Saurabh")
 
 
-let name = "hitesh"
+let name = "Saurabh"
 let age = 18
 let isLoggedIn = false
 let state;
